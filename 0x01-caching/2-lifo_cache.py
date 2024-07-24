68ce76041e94b65ea1d25eab3b608bd6a8a8a1a1
BelloAbdulmujib <belloabdulmujib3@gmil.com>
2024-07-24 10:57:20 -1100

    0x01-caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -26,7 +26,7 @@ class LIFOCache(BaseCaching):
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 final_key, other_key = self.cache_data.popitem(True)
-                print("DISCSRD:", final_key)
+                print("DISCARD:", final_key)
         self.cache_data[key] = item
         self.cache_data.move_to_end(key, last=True)
 
