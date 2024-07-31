f9b81a2543ea7b46fe41cc79cb818f9453fe5d3a
BelloAbdulmujib <belloabdulmujib3@gmil.com>
2024-07-30 13:45:32 -1100

    3-index.html

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -29,7 +29,7 @@ def get_locale() -> str:
 def index():
     """This returns the index page
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
